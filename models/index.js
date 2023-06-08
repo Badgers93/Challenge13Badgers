d6520df370cf5091bbd00dc20d523cefd6a68e8a
Badgers <brianbadger93@gmail.com>
2023-06-08 06:47:23 -0500

    created associations

--- models/index.js
+++ models/index.js
@@ -6,12 +6,16 @@ const ProductTag = require('./ProductTag');
 
 // Associations
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: 'category_id'});
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: 'category_id'});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
